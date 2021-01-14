b32cc4755214c28ca96349d3f7dbb2adc6ee655d
sa11616079 <satish.patel@successive.tech>
2021-01-14 15:03:44 +0530

    docs : new files feature/58283

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import schema from './module/index.js';
+import Server from './server.js';
+import configuration from './config/index.js';
+
+const server = new Server(configuration);
+
+server.bootstrap().setupApollo(schema);
---
+++ src/server.js
@@ -0,0 +1,35 @@
+import Express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+
+export default class Server {
+  constructor(config)
+  {
+    this.config = config;
+    this.app = new Express();
+    this.run = this.run.bind(this);
+  }
+  get application() {
+    return this.app;
+  }
+  bootstrap() {
+    return this;
+  }
+  run(){
+    const { port, nodeEnv } = this.config;
+    this.app.listen(port, () => {
+      console.info(`Server started on port ${port} (${nodeEnv})`);
+    });
+    return this;
+  }
+  async setupApollo(schema) {
+    const { app } = this;
+    this.server = new ApolloServer({
+      ...schema,
+      onHealthCheck: () => new Promise((resolve) => {
+        resolve('I am Okay...');
+      }),
+    });
+    this.server.applyMiddleware({ app });
+    this.run();
+  }
+}
