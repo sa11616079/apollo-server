39b7ff0c183800ead907777bec4b0eb203b324b6
sa11616079 <satish.patel@successive.tech>
2021-02-03 18:55:20 +0530

    docs : files final

--- src/module/trainee/mutation.js
+++ src/module/trainee/mutation.js
@@ -5,11 +5,11 @@ import constant from '../../libs/constants.js';
 
 export default {
   createTrainee: async (parent, args, context) => {
+    console.log('inside create');
     const { dataSources: { traineeAPI } } = context;
     const { payload: { name, email, password } } = args;
     const response = await traineeAPI.createTrainee({ name, email, password });
     const createdTrainee = JSON.stringify(response);
-    console.log('created data : ', createdTrainee);
     pubsub.publish(constant.subscriptions.TRAINEE_ADDED, { traineeAdded: createdTrainee });
     return createdTrainee;
   },
@@ -24,20 +24,16 @@ export default {
     const data = {
       originalId, name, email, role
     };
-    console.log('data :', data);
     const response = await traineeAPI.updateTrainee({ ...data });
-    const updatedTrainee = JSON.stringify(response);
-    console.log('trainee updated : ', updatedTrainee);
-    pubsub.publish(constant.subscriptions.TRAINEE_UPDATED, { traineeUpdated: updatedTrainee });
-    return updatedTrainee;
+    pubsub.publish(constant.subscriptions.TRAINEE_UPDATED, { traineeUpdated: response });
+    return response;
   },
   deleteTrainee: async (parent, args, context) => {
+    console.log('inside delete');
     const { dataSources: { traineeAPI } } = context;
     const { payload: { originalId } } = args;
     const response = await traineeAPI.deleteTrainee(originalId);
-    const deletedTrainee = JSON.stringify(response);
-    console.log('trainee deleted : ', response);
-    pubsub.publish(constant.subscriptions.TRAINEE_DELETED, { traineeDeleted: deletedTrainee });
-    return deletedTrainee;
+    pubsub.publish(constant.subscriptions.TRAINEE_DELETED, { traineeDeleted: response });
+    return response;
   }
 };
